4704de53f5c02d928d87d4393a3c6224583bd0af
drewbuck <dbuck182@gmail.com>
2026-02-13 12:25:28 -0500

    Looks good for now. Need to try to do drag and drop next

--- resources/js/pages/projects/ProjectGrid.tsx
+++ resources/js/pages/projects/ProjectGrid.tsx
@@ -20,48 +20,60 @@ export default function ProjectGrid({project, issues}: ProjectGridProps) {
     })
     return (
         <div className="flex h-full flex-1 flex-col gap-4 overflow-x-auto rounded-xl p-4">
-            <CreateIssueForm project_id={project.id}/>
                         <div className="grid auto-rows-min gap-4 md:grid-cols-4">
                             
-                            <div className="relative aspect-video overflow-hidden rounded-xl border border-sidebar-border/70 dark:border-sidebar-border flex flex-col items-center">
+                            <div className="relative rounded-xl border border-sidebar-border/70 dark:border-sidebar-border flex flex-col items-center p-2 gap-1">
                             <h2>Todo</h2>
+                            <div className='flex flex-col'>
                                 {sorted_issues[Status.Todo].map((i) => (
                                     <Link key={i.id}
                                         href={`/projects/${project.id}/issues/${i.id}`}>
                                         <IssueCard issue={i} />
                                     </Link>
                                 ))}
+                            </div>
+                                
                                 {/* <h2 className='text-center'>Add Pro</h2> */}
                                 {/* <PlaceholderPattern className="absolute inset-0 size-full stroke-neutral-900/20 dark:stroke-neutral-100/20" /> */}
                             </div>
-                            <div className="relative aspect-video overflow-hidden rounded-xl border border-sidebar-border/70 dark:border-sidebar-border flex flex-col items-center">
+                            <div className="relative rounded-xl border border-sidebar-border/70 dark:border-sidebar-border flex flex-col items-center p-2 gap-1">
                                 <h2>In Progress</h2>
-                                 {sorted_issues[Status.InProgress].map((i) => (
+                                <div className='flex flex-col'>
+                                    {sorted_issues[Status.InProgress].map((i) => (
                                     <Link key={i.id}
                                         href={`/projects/${project.id}/issues/${i.id}`}>
                                         <IssueCard issue={i} />
                                     </Link>
                                 ))}
+                                </div>
+                                 
                             </div>
-                            <div className="relative aspect-video overflow-hidden rounded-xl border border-sidebar-border/70 dark:border-sidebar-border flex flex-col items-center">
+                            <div className="relative rounded-xl border border-sidebar-border/70 dark:border-sidebar-border flex flex-col items-center p-2 gap-1">
                                 <h2>Review</h2>
-                                {sorted_issues[Status.Review].map((i) => (
+                                <div className='flex flex-col'>
+                                    {sorted_issues[Status.Review].map((i) => (
                                     <Link key={i.id}
                                         href={`/projects/${project.id}/issues/${i.id}`}>
                                         <IssueCard issue={i} />
                                     </Link>
                                 ))}
+                                </div>
+                                
                             </div>
-                            <div className="relative aspect-video overflow-hidden rounded-xl border border-sidebar-border/70 dark:border-sidebar-border flex flex-col items-center">
+                            <div className="relative rounded-xl border border-sidebar-border/70 dark:border-sidebar-border flex flex-col items-center p-2 gap-1">
                                 <h2>Done</h2>
-                                {sorted_issues[Status.Done].map((i) => (
+                                <div className='flex flex-col'>
+                                    {sorted_issues[Status.Done].map((i) => (
                                     <Link key={i.id}
                                         href={`/projects/${project.id}/issues/${i.id}`}>
                                         <IssueCard issue={i} />
                                     </Link>
                                 ))}
+                                </div>
+                                
                             </div>
                         </div>
+                        <CreateIssueForm project_id={project.id}/>
                         {/* <div className="relative min-h-[100vh] flex-1 overflow-hidden rounded-xl border border-sidebar-border/70 md:min-h-min dark:border-sidebar-border">
                             <PlaceholderPattern className="absolute inset-0 size-full stroke-neutral-900/20 dark:stroke-neutral-100/20" />
                         </div> */}
